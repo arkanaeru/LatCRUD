d08b77bfd92c3a4027f1a00fd6b6721908eac94d
arkanaeru <161271443+arkanaeru@users.noreply.github.com>
2024-05-07 02:33:30 +0700

    Add files via upload

---
+++ mhs.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 30, 2024 at 04:22 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `latihan1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhs`
+--
+
+CREATE TABLE `mhs` (
+  `nama` text NOT NULL,
+  `npm` varchar(20) NOT NULL,
+  `prodi` varchar(50) NOT NULL,
+  `no_telp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mhs`
+--
+
+INSERT INTO `mhs` (`nama`, `npm`, `prodi`, `no_telp`) VALUES
+('Arka', '081', 'SISTEM INFORMASI', '0888'),
+('Arka', '081', 'SISTEM INFORMASI', '0888'),
+('Arka', '081', 'SISTEM INFORMASI', '0888'),
+('arkuy', '081', 'SISTEM INFORMASI', '0888');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
